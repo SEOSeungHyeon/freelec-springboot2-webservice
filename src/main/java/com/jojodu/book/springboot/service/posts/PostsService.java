058d6d8cceff2f4507af61a8eb7bcfd1b88c9ff0
ssh <seunghyeon0128@gamil.com>
2022-12-16 22:05:31 +0900

    Posts save 기능

---
+++ src/main/java/com/jojodu/book/springboot/service/posts/PostsService.java
@@ -0,0 +1,18 @@
+package com.jojodu.book.springboot.service.posts;
+
+import com.jojodu.book.springboot.domain.posts.PostsRepository;
+import com.jojodu.book.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
