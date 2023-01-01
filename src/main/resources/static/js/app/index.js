394e048f3ed3d9e013cbb4454e5743208debb3ec
ssh <seunghyeon0128@gamil.com>
2023-01-01 20:48:25 +0900

    게시글 수정 기능 추가

--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -1,8 +1,11 @@
 var main = {
     init : function() {
         var _this = this;
-        $('#btn-save').on('click', function() {
+        $('#btn-save').on('click', function () {
             _this.save();
+        });
+        $('#btn-update').on('click', function() {
+            _this.update();
         })
     },
     save : function() {
@@ -20,6 +23,27 @@ var main = {
             data: JSON.stringify(data)
         }).done(function () {
             alert('글이 등록되었습니다.');
+            window.location.href = '/';
+        }).fail(function (error) {
+            alert(JSON.stringify(error));
+        });
+    },
+    update: function() {
+        var data = {
+            title: $('#title').val(),
+            content: $('#content').val()
+        };
+
+        var id = $('#id').val();
+
+        $.ajax({
+            type: 'PUT',
+            url: '/api/v1/posts/' + id,
+            dataType: 'json',
+            contentType: 'application/json; charset=UTF-8',
+            data: JSON.stringify(data)
+        }).done(function () {
+            alert('글이 수정되었습니다.')
             window.location.href = '/';
         }).fail(function (error) {
             alert(JSON.stringify(error));
